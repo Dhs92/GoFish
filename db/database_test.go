b9783531a9d49e59e72e6973c1e3a499781fb1e6
Alex Forehand <forehand.alex@gmail.com>
2025-03-16 15:46:38 -0700

    Added new tests

--- db/database_test.go
+++ db/database_test.go
@@ -2,11 +2,12 @@ package db_test
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/Dhs92/GoFish/db"
+	"github.com/Dhs92/GoFish/logger"
 	"github.com/matthewhartstonge/argon2"
+	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
@@ -38,6 +39,7 @@ func setupMongoContainer(t *testing.T) (testcontainers.Container, string) {
 }
 
 func TestConnect(t *testing.T) {
+	logger.InitLogger()
 	mongoC, uri := setupMongoContainer(t)
 	defer mongoC.Terminate(context.Background())
 
@@ -48,6 +50,7 @@ func TestConnect(t *testing.T) {
 }
 
 func TestCreateCollections(t *testing.T) {
+	logger.InitLogger()
 	mongoC, uri := setupMongoContainer(t)
 	defer mongoC.Terminate(context.Background())
 
@@ -65,6 +68,7 @@ func TestCreateCollections(t *testing.T) {
 }
 
 func TestCreateIndexes(t *testing.T) {
+	logger.InitLogger()
 	mongoC, uri := setupMongoContainer(t)
 	defer mongoC.Terminate(context.Background())
 
@@ -78,6 +82,7 @@ func TestCreateIndexes(t *testing.T) {
 }
 
 func TestCreateUser(t *testing.T) {
+	logger.InitLogger()
 	mongoC, uri := setupMongoContainer(t)
 	defer mongoC.Terminate(context.Background())
 
@@ -93,7 +98,7 @@ func TestCreateUser(t *testing.T) {
 
 	user, _ := db.NewUser("John Doe", "test@test.com", string(passwordHash))
 
-	result, err := mockDatabase.Create(ctx, user)
+	result, err := mockDatabase.Create(ctx, user, "users")
 	require.NoError(t, err)
 	require.NotNil(t, result)
 
@@ -104,11 +109,74 @@ func TestCreateUser(t *testing.T) {
 	err = newUser.Decode(&userResult)
 
 	if err != nil {
-		fmt.Println("No user found")
-		fmt.Printf("%v\n", userResult)
+		log.Error().Err(err).Msg("No user found")
+		log.Error().Interface("user", userResult).Msg("User result")
 		require.NoError(t, err)
 	} else {
-		fmt.Println("User found")
-		fmt.Printf("%v\r\n", userResult)
+		log.Info().Msg("User found")
+		log.Info().Interface("user", userResult).Msg("User result")
 	}
+}
+
+func TestUpdateUser(t *testing.T) {
+	logger.InitLogger()
+	mongoC, uri := setupMongoContainer(t)
+	defer mongoC.Terminate(context.Background())
+
+	ctx := context.Background()
+	mockDatabase, err := db.Connect(ctx, uri, "testdb")
+	require.NoError(t, err)
+
+	argon := argon2.DefaultConfig()
+
+	passwordHash, err := argon.HashEncoded([]byte("password"))
+	require.NoError(t, err)
+
+	user, _ := db.NewUser("John Doe", "test@test.com", string(passwordHash))
+	_, err = mockDatabase.Create(ctx, user, "users")
+	require.NoError(t, err)
+
+	updatedUser, _ := db.NewUser("John Doe", "newemail@test.com", string(passwordHash))
+	filter := bson.M{"_id": user.ObjectID()}
+	update := bson.M{"$set": bson.M{"email": updatedUser.Email}}
+
+	result, err := mockDatabase.Update(ctx, filter, update, "users")
+	require.NoError(t, err)
+	require.NotNil(t, result)
+
+	newUser := mockDatabase.FindDocument(ctx, bson.M{"_id": user.ObjectID()}, "users")
+	require.NoError(t, err)
+
+	var userResult db.User
+	err = newUser.Decode(&userResult)
+	require.NoError(t, err)
+	assert.Equal(t, "newemail@test.com", userResult.Email)
+}
+
+func TestDeleteUser(t *testing.T) {
+	logger.InitLogger()
+	mongoC, uri := setupMongoContainer(t)
+	defer mongoC.Terminate(context.Background())
+
+	ctx := context.Background()
+	mockDatabase, err := db.Connect(ctx, uri, "testdb")
+	require.NoError(t, err)
+
+	argon := argon2.DefaultConfig()
+
+	passwordHash, err := argon.HashEncoded([]byte("password"))
+	require.NoError(t, err)
+
+	user, _ := db.NewUser("John Doe", "test@test.com", string(passwordHash))
+	_, err = mockDatabase.Create(ctx, user, "users")
+	require.NoError(t, err)
+
+	filter := bson.M{"_id": user.ObjectID()}
+	result, err := mockDatabase.Delete(ctx, filter, "users")
+	require.NoError(t, err)
+	require.NotNil(t, result)
+
+	deletedUser := mockDatabase.FindDocument(ctx, bson.M{"_id": user.ObjectID()}, "users")
+	err = deletedUser.Decode(&db.User{})
+	assert.Error(t, err)
 }
